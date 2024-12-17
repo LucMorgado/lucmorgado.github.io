ccac5160fdf7a5b15a571d53d6f5185d7b54d644
Lucas Morgado <57923497+LucMorgado@users.noreply.github.com>
2024-12-16 21:12:43 -0300

    controle_recebimento V1.0.0

---
+++ bayer/1/usuarios.js
@@ -0,0 +1,24 @@
+const usuarios = [
+
+	{
+		"nome": "Rafael Bayer",
+		"matricula": "0140000",
+		"senha": "123456"
+
+	}
+
+	{
+		"nome": "Lucas Morgado",
+		"matricula": "0140001",
+		"senha": "123"
+
+	}
+
+	{
+		"nome": "José Melo",
+		"matricula": "0140002",
+		"senha": "ABC"
+
+	}
+
+];
