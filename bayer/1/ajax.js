427fac3c53b9bc3512e7ca89be4ea9ab52a0d74a
Lucas Morgado <lucmorgado@outlook.com>
2025-02-28 00:04:00 -0300

    Adicionando Banco

---
+++ bayer/1/ajax.js
@@ -0,0 +1,56 @@
+        alert("ok");
+
+/**
+  * Função para criar um objeto XMLHTTPRequest
+  */
+ function CriaRequest() {
+     try{
+         request = new XMLHttpRequest();
+     }catch (IEAtual){
+
+         try{
+             request = new ActiveXObject("Msxml2.XMLHTTP");
+         }catch(IEAntigo){
+
+             try{
+                 request = new ActiveXObject("Microsoft.XMLHTTP");
+             }catch(falha){
+                 request = false;
+             }
+         }
+     }
+
+     if (!request)
+         alert("Seu Navegador não suporta Ajax!");
+     else
+         return request;
+ }
+
+ /**
+  * Função para enviar os dados
+  */
+
+ function getDados(funcao, id) {
+     // Declaração de Variáveis
+     var result = document.getElementById(id);
+     var xmlreq = CriaRequest();
+
+     // Iniciar uma requisição
+     xmlreq.open("GET", "http://localhost/externo/controleUsuarios.php?funcao=" + funcao, true);
+
+     // Atribui uma função para ser executada sempre que houver uma mudança de ado
+     xmlreq.onreadystatechange = function(){
+
+         // Verifica se foi concluído com sucesso e a conexão fechada (readyState=4)
+         if (xmlreq.readyState == 4) {
+
+             // Verifica se o arquivo foi encontrado com sucesso
+             if (xmlreq.status == 200) {
+                 result.innerHTML = xmlreq.responseText;
+             }else{
+                 result.innerHTML = "Erro: " + xmlreq.statusText;
+             }
+         }
+     };
+     xmlreq.send(null);
+ }
