71ea176d1a9564fa3156026347eccacd4e417578
Lucas Morgado <57923497+LucMorgado@users.noreply.github.com>
2025-05-26 22:11:05 -0300

    Vsimples

---
+++ bayer2/1/functions.js
@@ -0,0 +1,20 @@
+
+
+	var showDrop = false;
+	function drop(){
+		var dropmenu = document.getElementById('dropmenu');
+
+		if(showDrop){
+			dropmenu.style = "display:none";
+			showDrop = false;
+		}else{
+			dropmenu.style = "display:flex";
+			showDrop = true;
+		}
+
+	}
+
+
+function sair(){
+	localStorage.removeItem("usuario");
+}
---
+++ bayer2/1/usuarios.js
@@ -0,0 +1,18 @@
+const usuarios = [
+
+	{
+		"nome": "ADM",
+		"matricula": "805",
+		"senha": "123",
+		"dashboard": true,
+		"gerenciar": true,
+		"triagem": true,
+		"alocar": true,
+		"divergencia": true,
+		"relatorio": true
+
+	}
+
+];
+
+
